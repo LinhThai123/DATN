b21aecbaa44cc1356224e4ebc869b6fa8769cd61
ASUS\Linht <Linhthai130100@gmail.com>
2024-03-06 12:21:03 +0700

    delete: Xóa color va Capacity

--- src/main/java/fithou/edu/vn/DoAnTotNghiep/product/query/getAllColors/GetAllColorQuery.java
+++
@@ -1,9 +0,0 @@
-package fithou.edu.vn.DoAnTotNghiep.product.query.getAllColors;
-
-import fithou.edu.vn.DoAnTotNghiep.common.cqrs.IRequest;
-import fithou.edu.vn.DoAnTotNghiep.product.dto.ColorDto;
-
-import java.util.Collection;
-
-public class GetAllColorQuery implements IRequest<Collection<ColorDto>> {
-}
